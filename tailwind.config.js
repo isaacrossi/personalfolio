751fc68fc6cca62e8f58d11618ebadc95d3b14df
Isaac Rossi <isaacjohnrossi@gmail.com>
2024-02-20 18:29:59 +1100

    added in assets and custom properties to the tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,25 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
+    fontFamily: {
+      body: "RightGrotesk-Medium",
+      heading: "RightGrotesk-TallBold",
+    },
+    fontSize: {
+      sm: "0.875rem",
+      base: "1rem",
+      xl: "1.618rem",
+      "2xl": "2.618rem",
+      "3xl": "4.236rem",
+      "4xl": "6.854rem",
+      "5xl": "11.089rem",
+    },
+    colors: {
+      dark: "#2C3639",
+      secondary: "#3F4E4F",
+      light: "#DCD7C9",
+      tertiary: "#E68333",
+    },
     extend: {},
   },
   plugins: [],
