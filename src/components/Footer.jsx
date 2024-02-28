dd3e087af17e5d7a9b3ebd477f7d630fd0ca9a27
Isaac Rossi <isaacjohnrossi@gmail.com>
2024-02-28 19:33:57 +1100

    finished styling the Footer component

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,11 +6,22 @@ const Footer = () => (
       <div className="col-span-4">
         <Logo className="mb-2 w-5" />
         <p className="mb-2">Isaac Rossi</p>
-        <p className="py-2 border-y text-light border-light">
+        <p className="py-2 border-y text-light border-light w-3/5">
           Frontend Developer, based in Sydney. ready for work. All rights
           reserved.
         </p>
       </div>
+      <div className="col-span-4 flex flex-col">
+        <a className="mb-2">Work</a>
+        <a className="mb-2">Musings</a>
+        <a className="">Contact</a>
+      </div>
+      <div className="col-span-4 flex flex-col">
+        <a className="mb-2">Github</a>
+        <a className="mb-2">LinkedIn</a>
+        <a className="">X</a>
+      </div>
+      <div className="col-span-4"></div>
     </div>
   </footer>
 );
